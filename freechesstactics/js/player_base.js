3109c6cc7583f4092e7d759116172a75c5303dda
AndSemenoff <semenoff@protonmail.com>
2019-11-23 16:04:32 +0300

    Chess tactics init commit

---
+++ freechesstactics/js/player_base.js
@@ -0,0 +1,8 @@
+var player_base = [
+{"name": "Nigel Short", fide_id: "400025"},
+{"name": "Anatoly Karpov", fide_id: "4100026"},
+{"name": "Boris Gelfand", fide_id: "2805677"},
+{"name": "Evgeniy Najer", fide_id: "4118987"},
+{"name": "Ivan Sokolov", fide_id: "14400030"},
+{"name": "Lajos Portisch", fide_id: "700037"},
+]
